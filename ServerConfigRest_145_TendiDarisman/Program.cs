d9814ed5b3c0c449aaa910f55bd422c234a11085
tendi darisman <50667278+ttendii@users.noreply.github.com>
2021-12-28 20:00:37 +0700

    Add project files.

---
+++ ServerConfigRest_145_TendiDarisman/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceRest_145_TendiDarisman;
+
+namespace ServerConfigRest_145_TendiDarisman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY));
+                hostObjek.Open();
+                Console.WriteLine("Server ready ..");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
